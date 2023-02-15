991b2e754d0954c9f7d4db447dc9a753eeb5f505
ruancx <2975701566@qq.com>
2023-02-15 16:16:11 +0800

    在公司更新代码2-14

--- all.py
+++ all.py
@@ -1,18 +1,18 @@
 import pytest
 
-# if __name__ == '__main__':
-    # pytest.main(['-vs'])
-    # 指定目录
+if __name__ == '__main__':
+    pytest.main(['-vs'])
+    # # 指定目录
     # pytest.main(['-vs', './testjk'])
-    # 指定用例
+    # # 指定用例
     # pytest.main(['-vs', './testjk/test_interface.py::test_04_func'])
-    # 失败用例重跑
+    # # 失败用例重跑
     # pytest.main(['-vs', './test', '--reruns=1'])
-    #  -x表示只要要一个用例报错，那么测试停止
+    #  # -x表示只要要一个用例报错，那么测试停止
     # pytest.main(['-vs', './test', '-x'])
-    # --maxfail=2 出现两个用例失败就停止。
+    # # --maxfail=2 出现两个用例失败就停止。
     # pytest.main(['-vs', './test', '--maxfail=2'])
-    # k: 根据测试用例的部分字符事指定测试用例。
+    # # k: 根据测试用例的部分字符事指定测试用例。
     # pytest.main(['-vs', './test', '-k=two'])
-    # -n设置多线程
+    # # -n设置多线程
     # pytest.main(['-vs', './test', '-n=2'])
