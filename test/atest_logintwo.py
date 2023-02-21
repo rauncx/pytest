d1dcddcc7256d0e10f25b71b8467fa172cb9e034
ruancx <2975701566@qq.com>
2023-02-21 16:45:57 +0800

    在公司更新代码2-21

--- test/atest_logintwo.py
+++ test/atest_logintwo.py
@@ -5,3 +5,6 @@ class TestLoginTwo:
 
     def test_02_two(self):
         print("pytesttwo")
+
+    def test_logintwo(self):
+        print("测试登录22222")
--- test/test_login.py
+++ test/test_login.py
@@ -3,5 +3,11 @@ import pytest
 
 class TestLogin:
 
+    def test_login(self):
+        print("测试登录")
+
     def test_01_vaili(self):
         print("pytest")
+
+    def test_py(self):
+        print("test123")
