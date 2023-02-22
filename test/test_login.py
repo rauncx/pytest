66797c47540836471549f52c163485035f677f5f
Your Name <you@example.com>
2023-02-22 23:44:09 +0800

    2-22在家里面提交一下代码

--- test/test_login.py
+++ test/test_login.py
@@ -3,14 +3,19 @@ import pytest
 
 class TestLogin:
 
+    @pytest.mark.smoke
     def test_login(self):
         print("测试登录")
 
+    login = "false"
+    @pytest.mark.skipif(login =="false", reason="未登录成功就跳过这个测试用例")
     def test_01_vaili(self):
         print("pytest")
 
+    @pytest.mark.skip("跳过这个测试用例")  # 跳过这个测试用例
     def test_py(self):
-        print("test123")
+        print("跳过这个测试用例")
 
-    def test_jial(self):
-        print("在家里面提交一下代码")
+    @pytest.mark.user
+    def test_user(self):
+        print("测试用户管理")
--- testjk/test_interface.py
+++ testjk/test_interface.py
@@ -8,4 +8,8 @@ class TestInterface:
     def test_03_jk(self):
         print('testjk')
 
+    @pytest.mark.smoke
+    def test_loginjk(self):
+        print("测试登录接口")
+
 
